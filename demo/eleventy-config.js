f117a87c8413b2616eef8ed4183adbc3938c6ac9
Philip Borenstein <pborenstein@gmail.com>
2020-05-09 22:05:59 -0400

    line numbers no

--- demo/eleventy-config.js
+++ demo/eleventy-config.js
@@ -2,8 +2,8 @@ const syntaxHighlight = require("../.eleventy.js");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight, {
-    alwaysWrapLineHighlights: true,
-    showLineNumbers: true,
+    alwaysWrapLineHighlights: false,
+    showLineNumbers: false,
     numberingBase: 1
   });
 
